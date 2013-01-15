aaf4af3d255cac5044eebdb5e31aa3e4254b1c9e
pandamicro <lphuabin@gmail.com>
2013-01-15 14:47:46 +0100

    Order logic modified

--- fuel/app/classes/controller/achat/order.php
+++ fuel/app/classes/controller/achat/order.php
@@ -3,36 +3,41 @@
 class Controller_Achat_Order extends Controller_Frontend
 {
     
-    public function before()
-    {        
-    	parent::before();
-    }
-
-	public function action_view()
+    public function action_view()
 	{
-	    $order = Model_Achat_Order::getCurrentOrder();
-	    $cart = false;
-	    $currency = false;
-	    if($order) { 
-	        $cart = $order->getCart();
-	        $products = $cart->getProducts();
-	        $currency = $cart->getCurrency();
-	    }
+	    // Get cart existed
+        $cart = Model_Achat_Cart::getCurrentCart();
 	    
-	    if(!$order || !$cart || count($products) == 0) {
+	    if($cart) {
+	        // Order the cart
+	        $order = Model_Achat_Order::orderCart($cart);
+	        
+	        if($order) { 
+	            $cart = $order->getCart();
+	            $products = $cart->getProducts();
+	            $currency = $cart->getCurrency();
+	        }
+	        
+	        if(!$order || !$cart || count($products) == 0) {
+	            $this->template->title = 'Commande - SEASON 13, Histoire Interactive | Feuilleton Multiplateforme | Livre Jeux | HTML5';
+	            $this->template->content = View::forge('achat/order/empty', $data);
+	        }
+	        else {
+	            $data = array(
+	                'order' => $order,
+	                'cart' => $cart,
+	                'products' => $products,
+	                'currency' => $currency,
+	            );
+	            View::set_global('user_adresse', $user_adresse);
+	            
+	            $this->template->title = 'Commande - SEASON 13, Histoire Interactive | Feuilleton Multiplateforme | Livre Jeux | HTML5';
+	        	$this->template->content = View::forge('achat/order/view', $data);
+	        }
+	    }
+	    else {
 	        $this->template->title = 'Commande - SEASON 13, Histoire Interactive | Feuilleton Multiplateforme | Livre Jeux | HTML5';
 	        $this->template->content = View::forge('achat/order/empty', $data);
 	    }
-	    
-	    $data = array(
-	        'order' => $order,
-	        'cart' => $cart,
-	        'products' => $products,
-	        'currency' => $currency,
-	    );
-	    View::set_global('user_adresse', $user_adresse);
-	    
-	    $this->template->title = 'Commande - SEASON 13, Histoire Interactive | Feuilleton Multiplateforme | Livre Jeux | HTML5';
-		$this->template->content = View::forge('achat/order/view', $data);
 	}
 }
